7cb9681096d0e78ead0c19da03917eb47fe94bea
AMcDowald <anthony.mcdowald@gmail.com>
2017-10-13 16:30:35 -0400

    ds630 for hw3 knn

--- ds630hw3.R
+++ ds630hw3.R
@@ -56,10 +56,55 @@ abline(plot(predict_lda$predict,predict_lda$Diagnosis,
             xlab="predicted",ylab="actual"),a=0,b=1)
 
 #knn inc
-train_data <- data_with_corr[train,]
-test_data <- data_with_corr[-train,]
-cl <- factor(train_data$Diagnosis)
-x<-knn(train_data, test_data, cl, k = 10, prob=TRUE)
-summary(x)
-data_with_corr
+#https://rstudio-pubs-static.s3.amazonaws.com/123438_3b9052ed40ec4cd2854b72d1aa154df9.html
+train <- 1:100
+train_data <- data_with_corr[train,][3:8]
+test_data <- data_with_corr[-train,][3:8]
+train.def <- factor(data_with_corr$Diagnosis[train])
+test.def <- factor(data_with_corr$Diagnosis[-train])
+
+knn.1 <-  knn(train_data, test_data, train.def, k=1)
+knn.5 <-  knn(train_data, test_data, train.def, k=5)
+knn.20 <- knn(train_data, test_data, train.def, k=20)
+
+100 * sum(test.def == knn.1)/100 
+100 * sum(test.def == knn.5)/100
+100 * sum(test.def == knn.20)/100
+
+table(knn.1 ,test.def)
+table(knn.5 ,test.def)
+table(knn.20 ,test.def)
+
+gc.bkup <- data_with_corr
+plot(train_data[,names(train_data)],
+     col=gc.bkup[-train,],
+     pch=as.numeric(train.def),
+     main="Predicted Default, by 1 Nearest Neighbors",cex.main=.95)
+
+points(test_data[,names(train_data)],
+       bg=gc.bkup[-train,],
+       pch=c(21,24)[as.numeric(knn.1)],cex=1.2,col=grey(.7))
+
+legend("bottomright",pch=c(1,16,2,17),bg=c(1,1,1,1),
+       legend=c("data 0","pred 0","data 1","pred 1"),
+       title="default",bty="n",cex=.8)
+
+legend("topleft",fill=c(4,3,6,2),legend=c(1,2,3,4),
+       title="installment %", horiz=TRUE,bty="n",col=grey(.7),cex=.8)
+#----------------
+plot(train_data[,names(train_data)[1:2]],
+     col=c(4,3,6,2)[gc.bkup[-train, "worst_radius"]],
+     pch=c(1,2)[as.numeric(train.def)],
+     main="Predicted Default, by 1 Nearest Neighbors",cex.main=.95)
+
+points(test_data[,names(train_data)[1:2]],
+       bg=c(4,3,6,2)[gc.bkup[-train,"worst_radius"]],
+       pch=c(21,24)[as.numeric(knn.1)],cex=1.2,col=grey(.7))
+
+legend("bottomright",pch=c(1,16,2,17),bg=c(1,1,1,1),
+       legend=c("data 0","pred 0","data 1","pred 1"),
+       title="default",bty="n",cex=.8)
+
+legend("topleft",fill=c(4,3,6,2),legend=c(1,2,3,4),
+       title="installment %", horiz=TRUE,bty="n",col=grey(.7),cex=.8)
 #svm
